d35b8348ab0043aba4bc87fc9f83a31306077a1b
Rahul Mallick <rahulmallick1126@gmail.com>
2023-03-07 10:06:04 +0530

    used one-state instead of multiple useState

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,22 +2,45 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  // handling multiple states like this is totally fine .
 
-  // handling multiple states like this is totally fine .  
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  // another way is to use one-state to update all three state instead doing it separately.
+
+  const [userInput, enteredInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+
+    enteredInput({
+      //but here when we do this we also need to take care of the other values because once the state of entered title re-render we will lost the other two intered value if we didn't preserve it.
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    enteredInput({
+      ...userInput,
+      enteredAmount: event.targe.value,
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+
+    enteredInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
   return (
     <form>
